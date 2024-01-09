8ba2fc20a528e294bfc8653f45a8b3a6de1dfbbb
Aman-Gupta-01 <155880399+Aman-Gupta-01@users.noreply.github.com>
2024-01-09 10:24:05 +0000

    data types and typeof

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert("hello") // we are using nodejs, not browser
+
+/*
+// Number = int
+// bigint = int(large)
+// string = *!*
+// boolean = true/false
+// null = standealone claue
+// underfined = ^!^
+// symbol = -_-
+    */
+// object
+
+let name = "Aman"
+let age = 21
+let isLoggedIn = false 
+
+console.log(typeof name)
+console.log(typeof "Aman")
+console.log(typeof 21)
+console.log(typeof age)
+console.log(typeof false)
+console.log(typeof isLoggedIn)
+console.log(typeof "kddua")
+console.log(typeof "false")
+
+
+console.log(typeof null) //lull type
+console.log(typeof undefined) //type undefined
