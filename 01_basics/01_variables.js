8b228da9f3d50510a5b97876b784e78648c6a45f
Aman-Gupta-01 <155880399+Aman-Gupta-01@users.noreply.github.com>
2024-01-09 09:39:49 +0000

    variable and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = "1234"
+let accountemail = "aman@gmail.com"
+var accountpassword = "xyz"
+accountcity = "haryana"
+
+// accountId = "334"n //notpossible
+
+accountemail = "Zuzuba@pupu.com"
+accountpassword = "0000"
+accountcity = "Delhi"
+
+console.table([accountId, accountemail, accountpassword, accountcity]);
